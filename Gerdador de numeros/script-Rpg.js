f1459a5656ea87585ca328bc7bee5b7e8c6802e9
Caio Lima Silva <caiolima3475@gmail.com>
2023-11-01 22:45:09 -0300

    first commit

---
+++ Gerdador de numeros/script-Rpg.js
@@ -0,0 +1,17 @@
+function gerar20()
+  {
+    var resp = document.getElementById('resp');
+    resp.innerHTML = Math.floor(20* Math.random() + 1);
+  }
+ 
+  function gerar10()
+  {
+    var resp1 = document.getElementById('resp1');
+    resp1.innerHTML = Math.floor(10* Math.random() + 1);
+  }
+ 
+  function gerar6()
+  {
+    var resp2 = document.getElementById('resp2');
+    resp2.innerHTML = Math.floor(6* Math.random() + 1);
+  }
